eb24a4b5a9c9f259604393bc73a8eca27f22a82f
biwek <biweks@gmail.com>
2014-02-01 14:27:12 +0000

    updated takeaway test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,28 +3,32 @@ require_relative '../lib/takeaway'
 # => +441988222016
 
 describe Takeaway do 
+	
+	context "Menu:" do 
+		it "should list dishes" do 
+			mishmash = Takeaway.new
+			expect(mishmash.list_dishes).to eq(["pasta", "dumplings", "burger"])
+		end
 
-	it "should list dishes" do 
-		mishmash = Takeaway.new
-		expect(mishmash.list_dishes).to eq(["pasta", "dumplings", "burger"])
+		it "should contain dishes with its price" do 
+			mishmash = Takeaway.new
+			expect(mishmash.menu).to eq([{dish: "pasta", price: 7.00}, {dish: "dumplings", price: 4.00}, {dish: "burger", price: 5.00}])
+		end
 	end
 
-	it "should contain dishes with its price" do 
-		mishmash = Takeaway.new
-		expect(mishmash.menu).to eq([{dish: "pasta", price: 7.00}, {dish: "dumplings", price: 4.00}, {dish: "burger", price: 5.00}])
-	end
+	context "Order:" do 
+		it "take an order and return its total" do 
+			mishmash = Takeaway.new
+			mishmash.order("pasta", 3)
+			expect(mishmash.total).to eq(21.0)
+		end
 
-	it "take an order and return its total" do 
-		mishmash = Takeaway.new
-		mishmash.order("pasta", 3)
-		expect(mishmash.total).to eq(21.0)
-	end
-
-	it "takes two dish order and return its total" do 
-		mishmash = Takeaway.new
-		mishmash.order("pasta", 3)
-		mishmash.order("dumplings", 2)
-		expect(mishmash.total).to eq(29.0)
+		it "takes two dish order and return its total" do 
+			mishmash = Takeaway.new
+			mishmash.order("pasta", 3)
+			mishmash.order("dumplings", 2)
+			expect(mishmash.total).to eq(29.0)
+		end
 	end
 
 
